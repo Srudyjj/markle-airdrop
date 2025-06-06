09b57c434671aa924fd6c092986d301443d34f38
srudyjj <srudyjj@gmail.com>
2025-06-06 21:01:25 +0200

    Fix formating

--- script/DeployMerkleAirdrop.s.sol
+++ script/DeployMerkleAirdrop.s.sol
@@ -7,10 +7,8 @@ import {BagelToken} from "../src/BagelToken.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract DeployMerkleAirdrop is Script {
-
     bytes32 public constant ROOT = 0xb1e815a99ee56f7043ed94e7e2316238187a59d85c211d06f9be7c5f94424aec;
     uint256 public constant TRANSFER_AMOUNT = 4 * 2500 * 1e18;
-
 
     function deployMarkleAirdrop() internal returns (MerkleAirdrop, BagelToken) {
         vm.startBroadcast();
@@ -31,9 +29,7 @@ contract DeployMerkleAirdrop is Script {
         return (merkleAirdrop, bagelToken);
     }
 
-
     function run() external returns (MerkleAirdrop, BagelToken) {
         return deployMarkleAirdrop();
     }
-
 }
